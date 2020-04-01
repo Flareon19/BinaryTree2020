e883c3d389c465d9baf553445aa9629c2dca32c2
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-04-01 13:20:53 +0530

    edit in BinaryTree class

--- src/definition/BinaryTree.java
+++ src/definition/BinaryTree.java
@@ -11,11 +11,101 @@ import adt.BinaryTreeADT;
 
 public class BinaryTree<E> implements BinaryTreeADT<E> {
 
+    int countRight = 0;
     private Node<E> root;
+    private int numberOfNodes = 0;
 
     @Override
     public boolean add(E data) {
-        return false;
+        root = addRecursive(root, data);
+        numberOfNodes++;
+        return true;
+    }
+
+    private Node<E> addRecursive(Node<E> currentNode, E data) {
+        if (currentNode == null) {
+            return new Node<>(data);
+        }
+        if ((Integer)data < (Integer)currentNode.getData()) {
+            currentNode.leftChild = addRecursive(currentNode.getLeftChild(), data);
+        } else if ((Integer)data > (Integer)currentNode.getData()) {
+            currentNode.rightChild = addRecursive(currentNode.getRightChild(), data);
+        }
+        return currentNode;
+    }
+
+    public Node<E> remove(E data) {
+        root = deleteRecursive(root, data);
+        return root;
+    }
+
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int size() {
+        return numberOfNodes;
+    }
+
+    private Node<E> deleteRecursive(Node<E> currentNode, E data) {
+        if (currentNode == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> temp = currentNode;
+        if (currentNode.getData().equals(data)) {
+            if (currentNode.getLeftChild() == null && currentNode.rightChild == null)
+                return temp;
+            else {
+
+            }
+        }
+        if ((Integer)data < (Integer)currentNode.getData())
+            containsNodeRecursive(currentNode.getLeftChild(), data);
+        else if ((Integer)data > (Integer)currentNode.getData())
+            containsNodeRecursive(currentNode.getRightChild(), data);
+        return null;
+    }
+
+    public boolean containsNodeRecursive(Node<E> curentNode, E data) {
+        if (curentNode == null)
+            return false;
+        if (curentNode.getData().equals(data))
+            return true;
+        return (Integer)data < (Integer)curentNode.getData() ?
+                containsNodeRecursive(curentNode.getLeftChild(), data) : containsNodeRecursive(curentNode.getRightChild(), data);
+    }
+
+    private void traversePreOrder(Node<E> currentNode) {
+        if (currentNode != null) {
+            visit(currentNode.getData());
+            traversePreOrder(currentNode.getLeftChild());
+            traversePreOrder(currentNode.getRightChild());
+        }
+    }
+
+    private void traverseInOrder(Node<E> currentNode) {
+        if (currentNode != null) {
+            traverseInOrder(currentNode.getLeftChild());
+            visit(currentNode.getData());
+            traverseInOrder(currentNode.getRightChild());
+        }
+    }
+
+    private void traversePostOrder(Node<E> currentNode) {
+        if (currentNode != null) {
+            traversePostOrder(currentNode.getLeftChild());
+            traversePostOrder(currentNode.getRightChild());
+            visit(currentNode.getData());
+        }
+    }
+
+    private void visit(E data) {
+        System.out.print(data+" ");
+    }
+
+    public void print() {
+        traverseInOrder(root);
     }
 
     private static class Node<E> {
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,18 @@
 
 package main;
 
+import definition.BinaryTree;
+
 public class Main {
     public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        for (int i = 0; i < 10; i++) {
+            int randomNmber =(int)(Math.random()*100);
+            binaryTree.add(randomNmber);
+            System.out.print(randomNmber+" ");
+        }
+        System.out.println();
+        binaryTree.print();
 
     }
 }
